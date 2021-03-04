e9e2ad125e64783bc318ce5a9acd119155f54252
Colin Bradley <colinbradley68@gmail.com>
2021-03-04 09:45:27 -0500

    Create cryptos.py

---
+++ cryptos.py
@@ -0,0 +1,34 @@
+import urllib.request
+import requests
+import pandas as pd
+import json
+
+"""
+extracting all the data points in the dictionary for 1 day timeframe
+"""
+url1d = "https://api.nomics.com/v1/currencies/ticker?key=f7a0286fa28cadcf41c2dece7ec73010&ids=AAVE,ALGOO,BAND,BTC,COMP,CRV,ETH,LINK,MKR,OMG,REN,SNX,UMA,UNI,YFI&interval=1d"
+data = requests.get(url1d)
+data = data.json()
+data = json.dumps(data)
+data = json.loads(data)
+
+"""
+Not even going to explain this loop lol 
+"""
+for i in data: 
+    oned = i.pop("1d")
+    i.update(oned)
+df = pd.DataFrame(data)
+    
+print(df)
+
+df.to_csv("cryptos.csv")
+
+"""
+Machine Learning starts here
+"""
+
+#df["Price Change precentage"] = df["Price Change percentage"].astype(float)
+#df["Volume Change Percent"] = df["Volume Change Percent"].astype(float)
+#print(df)
+#print(data.json())
